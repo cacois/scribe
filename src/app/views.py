057c46867cac0667af169645575af1aebc4f4e13
C. Aaron Cois <aaron.cois@gmail.com>
2014-11-19 23:56:12 -0500

    starting to implement config update post

--- src/app/views.py
+++ src/app/views.py
@@ -1,30 +1,46 @@
 from app import app
 from flask.ext.pymongo import PyMongo
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-render_template, flash, jsonify
+render_template, flash, jsonify, json
 
 mongo = PyMongo(app)
 
-@app.route('/_get_config')
-def add_numbers():
-    config = mongo.db.config.find_one({},{'_id': False})
-    return jsonify(config)
+@app.route('/_config', methods=['GET', 'POST'])
+def config():
+    if request.method == 'POST' and request.headers['Content-Type'] == 'application/json':
+        app.logger.info('Config posted: ' + json.dumps(request.json));
+        # use $set to update the active config record without removing
+        # existing non-updated values
+        mongo.db.config.update({'active': True}, {
+            "$addToSet": {'categories': request.json.categories},
+            "$addToSet": {'users': request.json.users},
+            "$addToSet": {'tags': request.json.tags}
+            },upsert=True)
+    elif request.method == 'GET':
+        # get config form DB
+        cursor = mongo.db.config.find({'active': True},{'_id': False})
+        json_config = [doc for doc in cursor]
+        config = json_config[0]
+
+        app.logger.info('Returning config: ' + json.dumps(config))
+        return jsonify(config)
+    else:
+        return "Unsupported request header: " + request.method
 
 @app.route("/")
-def hello():
+def index():
     online_users = mongo.db.users.find({'online': True})
     return render_template('index.html',
         online_users=online_users)
 
-@app.route("/activities/:user", methods=['GET'])
-def user_activities():
-    return "Not Yet Implemented"
-
 @app.route("/activities", methods=['GET', 'POST'])
 def activities():
-    config = mongo.db.config.find({},{'categories': True})
-    return render_template('activities.html',
-        appconfig=config)
+    if request.method == 'POST':
+        app.logger.info('Received POST of activities: ' + request.json)
+    elif request.method == 'GET':
+        return render_template('activities.html')
+    else:
+        return "Unsupported request header: " + request.method
 
 @app.route("/reports", methods=['GET'])
 def report():
